11e49ac903aed7df02eec45632a37bb7ab5945aa
ArchZak <izear21@gmail.com>
2026-05-05 23:31:07 -0500

    chore: git ignore update + base server code

---
+++ src/my_server/server.py
@@ -0,0 +1,6 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP(name="Totally-not-a-Bot", instructions="An mcp for a discord bot that is definitely not a bot.")
+
+if __name__ == "__main__":
+    mcp.run()
