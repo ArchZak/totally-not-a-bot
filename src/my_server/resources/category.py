a37bb106d3f48f8f909330655d7a96d13ae08192
ArchZak <izear21@gmail.com>
2026-05-07 21:23:59 -0500

    feat: added resource to mcp entry

--- src/my_server/resources/category.py
+++ src/my_server/resources/category.py
@@ -1,6 +1,8 @@
 from fastmcp import FastMCP
 
-FastMCP.resource("resource://list_categories", "GET") 
+FastMCP.resource("resource://list_categories", "GET")
+
+
 async def list_categories():
     """Fetch all categories and their channels"""
     pass
--- src/my_server/server.py
+++ src/my_server/server.py
@@ -1,9 +1,13 @@
 from fastmcp import FastMCP
 
+from .resources.category import list_categories
+
 mcp = FastMCP(
     name="Totally-not-a-Bot",
     instructions="An mcp for a discord bot that is definitely not a bot.",
 )
+
+mcp.add_resource(list_categories)
 
 if __name__ == "__main__":
     mcp.run()
