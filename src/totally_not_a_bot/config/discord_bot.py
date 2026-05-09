a3a560e6f38a574fe8df76e8b5b62a4a2166e1d5
ArchZak <izear21@gmail.com>
2026-05-08 22:31:18 -0500

    docs: todos

--- src/totally_not_a_bot/config/discord_bot.py
+++ src/totally_not_a_bot/config/discord_bot.py
@@ -3,4 +3,8 @@ import discord
 
 class TotallyNotABot(discord.Client):
     def __init__(self):
-        pass
+        super().__init__(intents=discord.Intents.default())
+
+
+# TODO investigate intents for discord.py
+# TODO discord bot to have heartbeats
