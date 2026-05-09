0b7dcd79bad2c5b34855260d1dfa639d86048ef4
ArchZak <izear21@gmail.com>
2026-05-08 22:30:59 -0500

    feat: launching discord bot and mcp on same proc

--- src/totally_not_a_bot/server.py
+++ src/totally_not_a_bot/server.py
@@ -1,35 +1,24 @@
 import asyncio
-import logging
 import os
-import sys
 
+from config.discord_bot import TotallyNotABot
+from dotenv import load_dotenv
 from fastmcp import FastMCP
 
-from .config.discord_bot import TotallyNotABot
-from .resources.category import list_categories
+# from resources.category import list_categories
 
 mcp = FastMCP(
     name="Totally-not-a-Bot",
     instructions="An mcp for a discord bot that is definitely not a bot.",
 )
 
-mcp.add_resource(list_categories)
+# mcp.add_resource(list_categories)
 
 client = TotallyNotABot()
 
-
-@mcp.on_startup()
-async def startup():
-    """Starts the discord bot when the MCP server starts."""
-    token = os.getenv("DISCORD_BOT_TOKEN")
-    if token:
-        # thread out the discord bot so it doesn't block the MCP server
-        asyncio.create_task(client.run(token))
-        logging.info("Discord bot task scheduled.")
-    else:
-        logging.error("DISCORD_BOT_TOKEN not found in environment variables.")
-        sys.exit(1)
+load_dotenv()
 
 
 if __name__ == "__main__":
+    asyncio.run(client.run(os.getenv("DISCORD_BOT_TOKEN")))
     mcp.run()
