bcc5f39da14aa07a4d05f18d23537284bcbbe71a
ArchZak <izear21@gmail.com>
2026-05-14 19:18:45 -0500

    feat: scaffolding for category services

---
+++ src/totally_not_a_bot/internals/services/category.py
@@ -0,0 +1,29 @@
+# region Category Resources
+
+
+def get_all_categories_info():
+    pass
+
+
+# endregion
+
+# region Category Tools
+
+
+def create_category(name: str):
+    pass
+
+
+def edit_category(category_id: int, new_name: str):
+    pass
+
+
+def delete_category(category_id: int):
+    pass
+
+
+def move_category(category_id: int, new_position: int):
+    pass
+
+
+# endregion
