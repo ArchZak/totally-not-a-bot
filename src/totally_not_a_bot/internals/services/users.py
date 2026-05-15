c4583e7f00bf5aaccc28c800ed6ee011d5f05500
ArchZak <izear21@gmail.com>
2026-05-14 19:23:27 -0500

    feat: scaffolding for users services

---
+++ src/totally_not_a_bot/internals/services/users.py
@@ -0,0 +1,21 @@
+# region User Resources
+
+
+def get_user_info(user_id: int):
+    pass
+
+
+def get_user_stats(user_id: int):
+    pass
+
+
+# endregion
+
+# region User Tools
+
+
+def send_direct_message(user_id: int, content: str):
+    pass
+
+
+# endregion
